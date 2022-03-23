3630f946e3a774831ae5d851b7cb18bdc6e760d2
Jonathan Cheng <jcheng461@gmail.com>
2022-03-23 18:56:27 +0900

    update function to succeed in a few early tests. Currently succeeded in checking if a number is divisible by 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 // Business Logic
 function boopBeep(number) {
-  console.log("The number entered is: " + number);
+  console.log("The number entered is: ");
+  // for (let i = 0; i <= number; i++) {
+  //   console.log("Number: " + number);
+  // }
+  let result = number / 3;
+  console.log("Number divided by 3 is: " + result);
+  if (result - Math.floor(result) !== 0) {
+    console.log("Number is not divisible by 3.");
+  } else {
+    console.log("Number is divisible by 3.");
+  }
 }
 
 // User Interface Logic
